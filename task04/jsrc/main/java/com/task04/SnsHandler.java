ad156093b8312b8d0807eb9ce057a7d93a98500d
sukalyan_chakraborty <sukalyan_chakraborty@epam.com>
2025-03-12 15:45:47 +0530

    committed

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -1,18 +1,13 @@
 package com.task04;
 
-import com.amazonaws.services.lambda.runtime.Context;
-
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import com.syndicate.deployment.annotations.events.SnsEventSource;
-
-import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-
-import com.syndicate.deployment.model.RetentionSetting;
-
 import java.util.HashMap;
-
 import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
 
 @LambdaHandler(
 
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -1,13 +1,13 @@
 package com.task04;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(
 		lambdaName = "sqs_handler",
